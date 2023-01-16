d4313624501d04b38121851b99de1bb68211f15d
yuhyeon <dbgus3577@gmail.com>
2023-01-16 21:35:16 +0900

    xml 수정, List Adapter 적용

--- app/src/main/java/com/blueland/kimonesheet/view/activity/MainActivity.kt
+++ app/src/main/java/com/blueland/kimonesheet/view/activity/MainActivity.kt
@@ -1,9 +1,50 @@
 package com.blueland.kimonesheet.view.activity
 
+import android.app.Activity
+import android.content.Intent
 import com.blueland.kimonesheet.R
 import com.blueland.kimonesheet.base.BaseActivity
 import com.blueland.kimonesheet.databinding.ActivityMainBinding
+import com.blueland.kimonesheet.model.MemoModel
+import com.blueland.kimonesheet.view.adapter.ListAdapter
+import com.blueland.kimonesheet.widget.extension.activityResultLauncher
+import java.util.*
 
-class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
+class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main), ListAdapter.ListListener {
 
+    private val launcher = activityResultLauncher {
+        if (it.resultCode == Activity.RESULT_OK) {
+            // TODO: DB 메모 리스트 가져오기 (갱신)
+        }
+    }
+
+    override fun initView() {
+        super.initView()
+        binding.apply {
+            val adapter = ListAdapter()
+            // TODO: DB 메모 리스트 가져오기
+            adapter.items = listOf(MemoModel("제목", "내용", Date(System.currentTimeMillis())), MemoModel("제목", "내용", Date(System.currentTimeMillis())))
+            adapter.listener = this@MainActivity
+            recyclerView.adapter = adapter
+        }
+    }
+
+    override fun initListener() {
+        super.initListener()
+        binding.apply {
+            btnWrite.setOnClickListener {
+                val intent = Intent(this@MainActivity, WriteActivity::class.java)
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+                launcher.launch(intent)
+            }
+
+            btnSearch.setOnClickListener {
+                // TODO: 검색
+            }
+        }
+    }
+
+    override fun itemOnClick(pos: Int, item: MemoModel) {
+        // TODO: 수정 화면 이동
+    }
 }
--- app/src/main/java/com/blueland/kimonesheet/view/activity/WriteActivity.kt
+++ app/src/main/java/com/blueland/kimonesheet/view/activity/WriteActivity.kt
@@ -1,9 +1,20 @@
 package com.blueland.kimonesheet.view.activity
 
+import android.app.Activity
 import com.blueland.kimonesheet.R
 import com.blueland.kimonesheet.base.BaseActivity
 import com.blueland.kimonesheet.databinding.ActivityWriteBinding
 
 class WriteActivity : BaseActivity<ActivityWriteBinding>(R.layout.activity_write) {
 
+    override fun initListener() {
+        super.initListener()
+        binding.apply {
+            btnSave.setOnClickListener {
+                // TODO: 저장
+                setResult(Activity.RESULT_OK)
+                finish()
+            }
+        }
+    }
 }
